d8c033224f11cf00dcfd942adc3bf95d6b9b0484
Harry <382961818@qq.com>
2019-11-27 21:34:11 +0800

    add

--- 3_Object_Oriented_Programming/02_Intro_to_OOP/08_Initialization_List/main.cpp
+++ 3_Object_Oriented_Programming/02_Intro_to_OOP/08_Initialization_List/main.cpp
@@ -1,19 +1,23 @@
 #include <assert.h>
 #include <string>
-
+#include <iostream>
 // TODO: Define class Person
 class Person
 {
-    public:
-        // TODO: Define a public constructor with an initialization list
-        Person(std::string s):name(s){}
-        // TODO: Define a public member variable: name
-        std::string name;
-}
+
+public:
+  // TODO: Define a public constructor with an initialization list
+  Person (std::string s) : name(s){}
+  // TODO: Define a public member variable: name
+  std::string name;
+};
 
 // Test
-int main() {
-  Person alice("Alice");
-  Person bob("Bob");
+int main()
+{
+  Person alice("alice");
+  Person bob("bob");
   assert(alice.name != bob.name);
+  std::cout<<alice.name;
+  return 0;
 }
