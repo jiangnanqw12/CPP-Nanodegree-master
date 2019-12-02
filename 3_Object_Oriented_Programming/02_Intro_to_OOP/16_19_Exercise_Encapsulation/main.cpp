4cd5af7713c7c1d81f350c32a835e05ca00d1d69
Harry <382961818@qq.com>
2019-12-02 09:34:51 +0800

    Harry

--- 3_Object_Oriented_Programming/02_Intro_to_OOP/16_19_Exercise_Encapsulation/main.cpp
+++ 3_Object_Oriented_Programming/02_Intro_to_OOP/16_19_Exercise_Encapsulation/main.cpp
@@ -2,9 +2,20 @@
 
 class BankAccount{
     private:
-        int number;
+        long int number;
         std::string name;
         float funds;
     public:
-        
+        void set_number(long int number);
+        void set_name(std::string name);
+        void set_funds(float funds);
+        long int get_number() const;
+        std::string get_name() const;
+        float get_funds() const;
+
 };
+
+void BankAccount::set_number(long int number)
+{
+    BankAccount::number=number;
+}
