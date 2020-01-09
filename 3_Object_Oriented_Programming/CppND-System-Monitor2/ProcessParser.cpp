1866046e22f6ffa9286dbbe559c91dd984fe29fc
jiangnanqw12 <382961818@qq.com>
2020-01-09 17:57:30 +0800

    add

---
+++ 3_Object_Oriented_Programming/CppND-System-Monitor2/ProcessParser.cpp
@@ -0,0 +1,30 @@
+
+#include<iostream>
+#include"ProcessParser.h"
+#include"util.h"
+
+using std::string;
+//Reading /proc/[PID]/status for memory status of specific process
+string ProcessParser::getVmSize(string pid)
+{
+    string line;
+    //Declaring search attribute for file
+    string name = "VmData";
+    string value;
+    float result;
+    // Opening stream for specific file
+    ifstream stream = Util::getStream((Path::basePath() + pid + Path::statusPath()));
+    while(std::getline(stream, line)){
+        // Searching line by line
+        if (line.compare(0, name.size(),name) == 0) {
+            // slicing string line on ws for values using sstream
+            istringstream buf(line);
+            istream_iterator<string> beg(buf), end;
+            vector<string> values(beg, end);
+            //conversion kB -> GB
+            result = (stof(values[1])/float(1024));//string to float
+            break;
+        }
+    }
+    return to_string(result);
+}
