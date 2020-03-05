ff356f60c19a5f3bb435702a5445f79bb58765cd
jiangnanqw12 <382961818@qq.com>
2020-03-05 20:28:33 +0800

    20200305

--- 3_Object_Oriented_Programming/System-Monitor-Student/src/processorS.cpp
+++
@@ -1,4 +0,0 @@
-#include "processor.h"
-#include "linux_parser.h"
-// TODO: Return the aggregate CPU utilization
-float Processor::Utilization() { return std::stof(LinuxParser::CpuUtilization()[0]); }
