b4186656995785dd7e75ecc66872da67f5d19e2d
jiangnanqw12 <382961818@qq.com>
2020-03-13 17:51:22 +0800

    master

--- 2_Foundations/CppND-Route-Planning-Project-Student/src/route_model.h
+++ 2_Foundations/CppND-Route-Planning-Project-Student/src/route_model.h
@@ -15,31 +15,38 @@ You will be filling out the classes in route_model.h and route_model.cpp over th
 #include "model.h"
 #include <iostream>
 
-class RouteModel : public Model {
+class RouteModel : public Model
+{
 
+public:
+  class Node : public Model::Node
+  {
   public:
-    class Node : public Model::Node {
-      public:
-        // Add public Node variables and methods here.
-        
-        Node(){}
-        Node(int idx, RouteModel * search_model, Model::Node node) : Model::Node(node), parent_model(search_model), index(idx) {}
-      
-      private:
-        // Add private Node variables and methods here.
-        int index;
-        RouteModel * parent_model = nullptr;
-    };
-    
-    // Add public RouteModel variables and methods here.
-    RouteModel(const std::vector<std::byte> &xml);  
-    std::vector<Node> path; // This variable will eventually store the path that is found by the A* search.
-    std::vector<Node> &sNodes()
-    {
-      return m_Nodes;
-    }
+    // Add public Node variables and methods here.
+
+    Node() {}
+    Node(int idx, RouteModel *search_model, Model::Node node) : Model::Node(node), parent_model(search_model), index(idx) {}
+    Node *parent = nullptr;
+    float h_value = std::numeric_limits<float>::max();
+    float g_value = 0.0;
+    bool visited = false;
+    std::vector<Node *> neighbors;
+
   private:
-    // Add private RouteModel variables and methods here.
-    std::vector<Node> m_Nodes;
-    
+    // Add private Node variables and methods here.
+    int index;
+    RouteModel *parent_model = nullptr;
+  };
+
+  // Add public RouteModel variables and methods here.
+  RouteModel(const std::vector<std::byte> &xml);
+  std::vector<Node> path; // This variable will eventually store the path that is found by the A* search.
+  std::vector<Node> &sNodes()
+  {
+    return m_Nodes;
+  }
+
+private:
+  // Add private RouteModel variables and methods here.
+  std::vector<Node> m_Nodes;
 };
