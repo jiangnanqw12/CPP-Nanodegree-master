802d9e345b740b647e5b3904e48318245bf241ab
jiangnanqw12 <382961818@qq.com>
2020-03-14 17:32:33 +0800

    add

--- 2_Foundations/CppND-Route-Planning-Project-Student/src/route_model.cpp
+++ 2_Foundations/CppND-Route-Planning-Project-Student/src/route_model.cpp
@@ -1,6 +1,13 @@
 #include "route_model.h"
 #include <iostream>
 
-RouteModel::RouteModel(const std::vector<std::byte> &xml) : Model(xml) {
-
+RouteModel::RouteModel(const std::vector<std::byte> &xml) : Model(xml)
+{
+    int count = 0;
+    for (auto i : this->Nodes())
+    {
+        Node n1(count, this, i);
+        count++;
+        this->m_Nodes.emplace_back(n1);
+    }
 }
