bbf5e4cc62b530bd631bff254ae33ea7df3965a4
jiangnanqw12 <382961818@qq.com>
2020-05-27 21:51:31 +0800

    add

---
+++ 4_Memory_Management/03_New_and_Delete/15_malloc/15_malloc.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+// Included library for memset
+#include <cstring>
+// included library for malloc
+#include <cstdlib>
+int main()
+{
+    int *ptr = NULL;
+    // Notice how pointer is allocated to single int memory space
+    // but later is redicerted to show to memory block
+    ptr = new (std::nothrow) int;
+    //  We can initialize memory block using malloc
+    //  declaring memory space for array of 4 integers
+    ptr = (int *)std::malloc(4 * sizeof(int));
+    for (int i = 0; i < 4; i++)
+    {
+        std::cout << "Address: "
+                  << &ptr[i] << " Value: " << ptr[i] << std::endl;
+    }
+    // every memory place is used for initialization of specific value
+    // here we are using letters for values, but casted to ints
+    for (int i = 0; i < 4; i++)
+    {
+        // sending address by reference
+        std::memset(&ptr[i], ('A' + i), sizeof(int));
+    }
+    // Printing state of our array
+    // we are converting int values to char to see what is happening with our values
+    for (int i = 0; i < 4; i++)
+    {
+        std::cout << "Address: " << &ptr[i]
+                  << " Value: " << char(ptr[i])
+                  << std::endl;
+    }
+}
