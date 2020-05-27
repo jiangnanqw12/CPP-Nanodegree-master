5bea16cbdd38e7969cc8556c3983465d656b62c9
jiangnanqw12 <382961818@qq.com>
2020-05-27 17:54:12 +0800

    add

--- 4_Memory_Management/03_New_and_Delete/07_09_new_delete_lab/9test.cpp
+++ 4_Memory_Management/03_New_and_Delete/07_09_new_delete_lab/9test.cpp
@@ -33,3 +33,108 @@ Steps 3
     Class destructor deletes root pointer
     Test everything in main
 */
+#include <iostream>
+struct node
+{
+public:
+    node(int initdata, node *leftN, node *rightN) : left(leftN), right(rightN), data(initdata) {}
+    ~node()
+    {
+        if (left != nullptr)
+        {
+            delete left;
+        }
+        if (right != nullptr)
+        {
+            delete right;
+        }
+    }
+
+    int data;
+    node *left;
+    node *right;
+};
+class binary_tree
+{
+private:
+    node *root;
+    void add(int newdata, node *ptr);
+    bool search(int targetdata, node *ptr);
+
+public:
+    binary_tree(int rootdata) { root = new node(rootdata, nullptr, nullptr); }
+    ~binary_tree() { delete root; }
+    void addData(int inputdata) { add(inputdata, root); }
+    bool searchItem(int item) { search(item, root); }
+    int getroot()
+    {
+        return root->data;
+    }
+};
+void binary_tree::add(int newdata, node *ptr)
+{
+    if (newdata > ptr->data)
+    {
+        if (ptr->right == nullptr)
+            ptr->right = new node(newdata, nullptr, nullptr);
+        else
+        {
+            add(newdata, ptr->right);
+        }
+    }
+    else
+    {
+        if (ptr->left == nullptr)
+            ptr->left = new node(newdata, nullptr, nullptr);
+        else
+        {
+            add(newdata, ptr->left);
+        }
+    }
+}
+bool binary_tree::search(int targetdata, node *ptr)
+{
+    if (ptr != nullptr && ptr->right == nullptr && ptr->left == nullptr)
+    {
+        if (targetdata == ptr->data)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    if (targetdata == ptr->data)
+        return true;
+    else
+    {
+        if (targetdata > ptr->data && ptr->right != nullptr)
+            search(targetdata, ptr->right);
+        else if (targetdata < ptr->data && ptr->left != nullptr)
+        {
+            search(targetdata, ptr->left);
+        }
+    }
+}
+int main()
+{
+    binary_tree tree(2);
+    tree.addData(10);
+    tree.addData(1);
+    tree.addData(20);
+    tree.addData(0);
+
+    std::cout << tree.searchItem(0) << std::endl;
+    std::cout << tree.searchItem(9) << std::endl;
+    tree.addData(11);
+    std::cout << tree.searchItem(11) << std::endl;
+    /* OUTPUT:
+        1
+        0
+        1
+    */
+
+    std::cout << tree.getroot() << std::endl;
+    return 0;
+}
