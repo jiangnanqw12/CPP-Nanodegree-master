50c54ae73fc868c0acb3e2c71e126f8897363d42
jiangnanqw12 <382961818@qq.com>
2020-07-25 13:12:30 +0800

    add

---
+++ 5_Concurrency/02_Passing_Data_Between_Threads/02_Promises_and_Future/4.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <thread>
+#include <future>
+#include <cmath>
+#include <memory>
+
+void divideByNumber(std::promise<double> &&prms, double num, double denom)
+{
+    try
+    {
+        if
+    }
+}
+
+int main()
+{
+    // create promise and future
+    std::promise<double> prms;
+    std::future<double> ftr = prms.get_future();
+
+    // start thread and pass promise as argument
+    double num = 42.0, denom = 0.0;
+    std::thread t(divideByNumber, std::move(prms), num, denom);
+
+    // retrieve result within try-catch-block
+    try
+    {
+
+        std::cout << "Result = " << result << std::endl;
+    }
+    catch (std::runtime_error e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+
+    // thread barrier
+    t.join();
+
+    return 0;
+}
